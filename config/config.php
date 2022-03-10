f24daa0e3038acebec3ba5bc4d8701135f4808f3
WebmasterComputron <webmastercomputron@gmail.com>
2022-03-10 10:33:01 -0500

    omnicanal

--- config/config.php
+++ config/config.php
@@ -4,10 +4,10 @@ $u = "$_SERVER[HTTP_HOST]";
 //define('URL','http://10.5.2.191:8080/cartimexed/');// ip local:puerto
 define('URL','http://'.$u.'/omnicanal/');// ip local:puerto
 
-define('HOST', '10.5.1.184');
+define('HOST', 'localhost');
 define('DB', 'omnicanal');
 define('USER', 'root');
-define('PASSWORD', 'Sistema.01');
+define('PASSWORD', '');
 define('CHARSET', 'utf32_spanish2_ci');
 
 
